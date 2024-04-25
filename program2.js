b0c7cd88aabef6df26b32819772b86737c881661
huzefa2345 <techsbr@gmail.com>
2024-04-25 13:58:44 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,6 +1,22 @@
 function smallestMissingPositiveInteger(nums) {
 
   // Implement the function smallest_missing_positive_integer
+  let i = 0;
+  while (i < nums.length) {
+      if (nums[i] > 0 && nums[i] <= nums.length && nums[nums[i] - 1] !== nums[i]) {
+          [nums[nums[i] - 1], nums[i]] = [nums[i], nums[nums[i] - 1]]; // Swap numbers
+      } else {
+          i++;
+      }
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+      if (nums[i] !== i + 1) {
+          return i + 1;
+      }
+  }
+
+  return nums.length + 1;
 
 }
 
