aabafb2bf2b957d379f1fc730712c76e79ede6c8
huzefa2345 <techsbr@gmail.com>
2024-04-25 13:58:28 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,5 +1,16 @@
 function longestSubstring(s) {
     // Implementation of longestSubstring function
+    let start = 0, maxLength = 0;
+    let map = new Map();
+
+    for(let i = 0; i < s.length; i++) {
+        if(map.has(s[i])) {
+            start = Math.max(map.get(s[i]), start);
+        }
+        maxLength = Math.max(maxLength, i - start + 1);
+        map.set(s[i], i + 1);
+    }
+    return maxLength;
 }
 
 module.exports = { longestSubstring };
